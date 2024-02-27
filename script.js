6f20fa7b16dd006d9fc60113cb77af166df8e030
Abdelrahmen Bentahar <abdelrahmenbentahar@gmail.com>
2024-02-27 10:45:16 +0100

    Création de la première partie du site

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ for(icone of icones){
         e.preventDefault()
         e.stopPropagation()
         if(confirm(`Vous voulez accéder au ${e.originalTarget.alt}`)){
-            console.log(e)
+            if(e.originalTarget.alt == "facebook"){
+                var url = "https://facebook.com"
+            }else if(e.originalTarget.alt == "twitter"){
+                var url = "https://twitter.com"
+            }else if(e.originalTarget.alt == "instagram"){
+                var url = "https://instagram.com"
+            }
+            location.replace(url)
         }
     })
 }
